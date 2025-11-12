098e35badfebab3e32a3a0bd646010b648471529
Dashrath Deshmukh <rohan9503473615@gmail.com>
2025-11-13 01:17:26 +0530

    array commit

---
+++ array/ReadArray.java
@@ -0,0 +1,13 @@
+public class ReadArray  
+{ 
+    public static void main(String[] args)  
+    { 
+        int[] arr = {10,20,3,5,7}; 
+ 
+        System.out.println("Array elements are:"); 
+        for (int i = 0; i < arr.length; i++)  
+ { 
+            System.out.print(arr[i] + " "); 
+        } 
+    } 
+} 
